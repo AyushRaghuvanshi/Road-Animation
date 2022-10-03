47039fe6ada639328e2a714fbad37f4242021be5
Ayush <ayushraghuvanshi03@gmail.com>
2022-10-03 14:55:20 +0530

    ADD Car Navigation

--- main.js
+++ main.js
@@ -1,12 +1,68 @@
 let id = null;
-const elem = document.getElementsByClassName("stripes_group");
-let pos = 0;
-clearInterval(id);
-id = setInterval(frame, 100);
-function frame() {
-  if (pos == 30) {
-    pos = 0;
+var elem = document.getElementsByClassName("car");
+let Y = 0;
+let X = 0;
+let velocity = 5;
+let rotate = 0;
+let up = false,
+  end = true,
+  right = false,
+  left = false;
+elem[0].style.top = Y + "px";
+let down = false;
+id = setInterval(frame, 5);
+document.addEventListener("keydown", (event) => {
+  if (event.key === "ArrowLeft") {
+    left = true;
   }
-  pos++;
-  elem[0].style.transform = `translateY(${pos + "px"})`;
+  if (event.key === "ArrowRight") {
+    right = true;
+  }
+  if (event.key === "ArrowUp") {
+    up = true;
+  }
+  if (event.key === "ArrowDown") {
+    down = true;
+  }
+});
+
+document.addEventListener("keyup", (event) => {
+  if (event.key === "ArrowLeft") {
+    // rotate = 0;
+    left = false;
+  }
+  if (event.key === "ArrowRight") {
+    // rotate = 0;
+    right = false;
+  }
+  if (event.key === "ArrowUp") {
+    up = false;
+  }
+  if (event.key === "ArrowDown") {
+    down = false;
+  }
+});
+
+function frame() {
+  if (!end) {
+    clearInterval(id);
+  } else {
+    console;
+    elem[0].style.transform = `translate(${X}px,${Y}px) rotateZ(${-rotate}deg)`;
+    if (up) {
+      console.log(X, Y);
+      X -= velocity * Math.sin((3.14 / 180) * rotate);
+      Y -= velocity * Math.cos((3.14 / 180) * rotate);
+    }
+    if (down) {
+      X += velocity * Math.sin((3.14 / 180) * rotate);
+      Y += velocity * Math.cos((3.14 / 180) * rotate);
+    }
+    if (right) {
+      rotate--;
+    }
+    if (left) {
+      rotate++;
+    }
+  }
 }
